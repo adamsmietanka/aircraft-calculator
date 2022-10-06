2a5611ee821404850b09bba7f71d7329c257f290
B4rtSL <bartsl1029@gmail.com>
2022-10-07 01:29:09 +0200

    feat: add Slider component with active display

--- frontend/src/components/organisms/PowerUnitEngine.tsx
+++ frontend/src/components/organisms/PowerUnitEngine.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InputNumber from "../atoms/InputNumber";
+import SliderInput from "../atoms/SliderInput";
 import { useEngineStore } from "../../utils/useEngine";
 import PowerUnitEngineChart from "../molecules/PowerUnitEngineChart";
 
@@ -7,9 +8,11 @@ const PowerUnitEngine = () => {
     const seaLevelPower = useEngineStore((state) => state.seaLevelPower)
     const engineSpeed = useEngineStore((state) => state.engineSpeed)
     const reductionRatio = useEngineStore((state) => state.reductionRatio)
+    const maxAltitude = useEngineStore((state) => state.maxAltitude)
     const setSeaLevelPower = useEngineStore((state) => state.setSeaLevelPower)
     const setEngineSpeed = useEngineStore((state) => state.setEngineSpeed)
     const setReductionRatio = useEngineStore((state) => state.setReductionRatio)
+    const setMaxAltitude = useEngineStore((state) => state.setMaxAltitude)
   return (
     <div className="flex">
       <div className="flex flex-col w-64 mr-8 space-y-2">
@@ -30,6 +33,12 @@ const PowerUnitEngine = () => {
           setter={setReductionRatio}
           label="Reduction Ratio"
           unit=":1"
+        />
+        <SliderInput
+          value={maxAltitude}
+          setter={setMaxAltitude}
+          label="Maximum altitude"
+          unit="km"
         />
       </div>
       <PowerUnitEngineChart />
