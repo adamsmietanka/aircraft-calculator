2b077957b32c23814cd6824b7e194315b2885bc4
B4rtSL <102322573+B4rtSL@users.noreply.github.com>
2022-10-11 21:19:12 +0200

    feat: add slider component with active display (#1)
    * feat: add Slider component with active display
    
    * feat: unification of components styling, add steps
    
    * feat: add netlify config
    
    * feat: disabled dark mode
    
    * refactor: split navigation into components (#2)
    
    * feat: add navigation components
    
    * feat: add pages for the remaining sites
    
    * fix: fix exhaustive deps warning
    
    * feat: add homepage
    
    * feat: added basic defaults
    
    * feat: add Slider component with active display
    
    * feat: unification of components styling, add steps
    
    * feat: disabled dark mode
    
    * feat: added basic defaults
    
    Co-authored-by: adamsmietanka <adamsmietanka@outlook.com>
    Co-authored-by: Adam Śmietanka <42046760+adamsmietanka@users.noreply.github.com>

--- frontend/src/components/atoms/InputNumber.tsx
+++ frontend/src/components/atoms/InputNumber.tsx
@@ -1,27 +1,30 @@
-import React from "react";
-
 interface Props {
   label: string;
   unit: string;
   value: number;
+  step?: number;
+  min?: number;
   setter: (value: number) => void;
 }
 
-const InputNumber = ({label, unit, value, setter}: Props) => {
+const InputNumber = ({label, unit, value, step=1, min=0, setter}: Props) => {
   return (
     <div className="form-control">
       <label className="label">
         <span className="label-text">{label}</span>
       </label>
-      <label className="input-group">
+      <label className="input-group flex items-center">
         <input
+          className="input input-bordered w-48"
           type="number"
-          step={10}
-          className="input input-bordered"
+          step={step}
           value={value}
+          min={min}
           onChange={(e) => setter(parseFloat(e.target.value))}
         />
-        <span>{unit}</span>
+        <span className="flex items-center justify-center bg-gray-200 px-1 w-20 rounded-lg h-12">
+          {unit}
+        </span>
       </label>
     </div>
   );
