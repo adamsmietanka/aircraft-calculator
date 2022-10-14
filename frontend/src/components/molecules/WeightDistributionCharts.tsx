5a7797829af885eebf4b4375afe5c5a4d233fb2f
LukaszOgrodnik <ogrud2000@gmail.com>
2022-10-14 10:20:20 +0200

    feat: create WegihtDistro chart component for future development

---
+++ frontend/src/components/molecules/WeightDistributionCharts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeightDistributionCharts = () => {
+  return (
+    <div className="bg-red-200 w-full text-bold text-black text-3xl flex flex-col justify-center items-center">
+      <div>Charts go here</div>
+    </div>
+  );
+}
+
+export default WeightDistributionCharts
