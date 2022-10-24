c6a73940c4a95c0bfffa4eba2a060126d30efa55
LukaszOgrodnik <ogrud2000@gmail.com>
2022-10-24 17:43:34 +0200

    feat: be in the previous commit

--- frontend/src/utils/useWeightConfiguration.ts
+++ frontend/src/utils/useWeightConfiguration.ts
@@ -1,46 +1,59 @@
 import create from "zustand";
 
-interface WegihtComponent {
+interface WeightComponent {
   componentName: string;
   mass: number;
   cords: { x: number; y: number; z: number };
 }
 
 interface WeightConfigurationsState {
-  weightConfigurations: Array<
-    { name: string; components: Array<WegihtComponent>; enabled: boolean }>
-  ;
+  weightConfigurations: Array<{
+    name: string;
+    components: Array<WeightComponent>;
+  }>;
+  activeWeightConfiguration: {
+    name: string;
+    components: Array<WeightComponent>;
+  };
   setWeightConfigurations: (
-    value: Array<{ name: string; components: Array<WegihtComponent>; enabled: boolean }>
+    value: Array<{
+      name: string;
+      components: Array<WeightComponent>;
+    }>
   ) => void;
+  setActiveWeightConfiguration: (value: {
+    name: string;
+    components: Array<WeightComponent>;
+  }) => void;
 }
 
 const defaultComponents1 = [
   { componentName: "Fuselage", mass: 900, cords: { x: 0, y: 0, z: 0 } },
-  { componentName: "Wing", mass: 900, cords: { x: 0, y: 0, z: 0 } }
+  { componentName: "Wing", mass: 900, cords: { x: 0, y: 0, z: 0 } },
 ];
 
 const defaultComponents2 = [
   { componentName: "Fuselage", mass: 900, cords: { x: 0, y: 0, z: 0 } },
   { componentName: "Wing", mass: 900, cords: { x: 0, y: 0, z: 0 } },
-  { componentName: "Rudder", mass: 900, cords: { x: 0, y: 0, z: 0 } }
+  { componentName: "Rudder", mass: 900, cords: { x: 0, y: 0, z: 0 } },
 ];
 
 const defaultConfigurations = [
   {
     name: "Default Configuration 1",
     components: defaultComponents1,
-    enabled: true,
   },
   {
     name: "Default Configuration 2",
     components: defaultComponents2,
-    enabled: false,
   },
-]
+];
 
 export const useWeightStore = create<WeightConfigurationsState>()((set) => ({
   weightConfigurations: defaultConfigurations,
+  activeWeightConfiguration: defaultConfigurations[0],
   setWeightConfigurations: (value) =>
     set((state) => ({ weightConfigurations: value })),
+  setActiveWeightConfiguration: (value) =>
+    set((state) => ({ activeWeightConfiguration: value })),
 }));
