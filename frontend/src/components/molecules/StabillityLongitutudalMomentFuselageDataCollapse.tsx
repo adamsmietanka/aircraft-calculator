bd291161fb414194aa9847284779035eae691e4e
LukaszOgrodnik <ogrud2000@gmail.com>
2022-12-19 05:45:54 +0100

    feat: add stabilliy longitudonal moament inputs

--- frontend/src/components/molecules/StabillityLongitutudalMomentFuselageDataCollapse.tsx
+++ frontend/src/components/molecules/StabillityLongitutudalMomentFuselageDataCollapse.tsx
@@ -2,14 +2,84 @@ import React from 'react'
 import { useLongitudalMomentStore } from "../../utils/useLongitudalMoment";
 
 import InputNumber from "../atoms/InputNumber";
+import OverwriteCheckbox from '../atoms/OvrwriteCheckbox';
 const StabillityLongitutudalMomentFuselageDataCollapse = () => {
-
+  const data = useLongitudalMomentStore() 
   return (
     <div tabIndex={0} className="collapse border rounded-box">
     <input type="checkbox" />
     <button className="collapse-title text-xl font-medium"> Fuselage Longitudal Moment</button>
     <div className="collapse-content ">
 
+    <InputNumber
+          value={data.s_b}
+          setter={data.setS_b}
+          label="Top view fuselage surface"
+          unit="m^2"
+        />
+
+    <InputNumber
+          value={data.s_bf}
+          setter={data.setS_bf}
+          label="Front fuselage part top view surface"
+          unit="m^2"
+        />
+
+    <InputNumber
+          value={data.w}
+          setter={data.setW}
+          label="Fuselage width"
+          unit="m"
+        />
+
+    <InputNumber
+          value={data.l_b}
+          setter={data.setL_b}
+          label="Fuselage length"
+          unit="m"
+        />
+
+    <InputNumber
+          value={data.l_bf}
+          setter={data.setL_bf}
+          label="Fuselagwe front part lenght"
+          unit="m"
+        />
+
+    <InputNumber
+          value={data.i_w}
+          setter={data.setI_w}
+          label="Wing incilnation"
+          unit="rad"
+        />
+
+    <InputNumber
+          value={data.c_a}
+          setter={data.setC_a}
+          label="Wing mean aerodynamic cord"
+          unit="m"
+        />
+    <OverwriteCheckbox label ="Get wing mean aerodynamic cord value from previous calculation"/>
+    <InputNumber
+          value={data.S}
+          setter={data.setS}
+          label="Wing surface"
+          unit="m^2"
+        />
+    <OverwriteCheckbox label ="Get wing surface value from previous calculation"/>
+    <InputNumber
+          value={data.c_0}
+          setter={data.setC0}
+          label="Wing cord in the fuselage axis"
+          unit="m"
+        />
+    <OverwriteCheckbox label ="Get wing surface value from previous calculation"/>
+    <InputNumber
+          value={data.b_k}
+          setter={data.setb_k}
+          label="Width of the fuselage in the place where wings come"
+          unit="m"
+        />
     </div>
   </div>
   )
--- frontend/src/utils/useLongitudalMoment.ts
+++ frontend/src/utils/useLongitudalMoment.ts
@@ -1,6 +1,28 @@
 import create from 'zustand'
 
 interface LongitudalMomentState {
+  //Fuselage
+  s_b:number
+  s_bf:number
+  w:number
+  l_b:number
+  l_bf:number
+  c_a:number
+  i_w:number
+  S:number
+  c_0:number
+  b_k:number
+  setS_b:(value: number) => void
+  setS_bf:(value: number) => void
+  setW:(value: number) => void
+  setL_b:(value: number) => void
+  setL_bf:(value: number) => void
+  setC_a:(value: number) => void
+  setI_w:(value: number) => void
+  setS:(value: number) => void
+  setC0:(value: number) => void
+  setb_k:(value: number) => void
+  //Wing
   cm0p: number
   alfa: Array<number>
   cx: Array<number>
@@ -20,6 +42,28 @@ interface LongitudalMomentState {
 }
 
 export const useLongitudalMomentStore = create<LongitudalMomentState>()((set) => ({
+  //fuselage
+  s_b:2000,
+  s_bf:300,
+  w:4,
+  l_b:20,
+  l_bf:7,
+  c_a:3.4,
+  i_w:1,
+  S:20,
+  c_0:4,
+  b_k:12,
+  setS_b:(value) => set((state) => ({ cm0p: value })),
+  setS_bf:(value) =>set((state)=>({s_bf:value})),
+  setW:(value) =>set((state)=>({w:value})),
+  setL_b:(value) =>set((state)=>({l_b:value})),
+  setL_bf:(value) =>set((state)=>({l_bf:value})),
+  setC_a:(value) =>set((state)=>({c_a:value})),
+  setI_w:(value) =>set((state)=>({i_w:value})),
+  setS:(value) =>set((state)=>({S:value})),
+  setC0:(value) =>set((state)=>({c_0:value})),
+  setb_k:(value) =>set((state)=>({b_k:value})),
+  //wing
   cm0p: 1000,
   alfa:[0],
   cx: [3000],
