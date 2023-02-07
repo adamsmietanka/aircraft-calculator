c6e4da9f452ac15012b67db1dc0ee580ee0114bc
LukaszOgrodnik <ogrud2000@gmail.com>
2023-02-07 10:42:53 +0100

    refactor: implement equtions to estimate a1 and a2 coeficients eqatuions using diffrent file structure.

--- frontend/src/utils/steerCalculation/aCoefficients/a10T.ts
+++ frontend/src/utils/steerCalculation/aCoefficients/a10T.ts
@@ -1,5 +1,7 @@
 //Constatnts where estimated numerically
 //tau : [deg] ,  profileThickness: [-]
-export const a10T = (tau: number, profileThickness: number) => {
+const a10T = (tau: number, profileThickness: number) => {
   return (0.019 * tau + 4.67) * profileThickness + 6.28;
 };
+
+export default a10T
