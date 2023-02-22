837143f2725e7ad3f5cbaadc1460b7ee11a5a2ea
LukaszOgrodnik <ogrud2000@gmail.com>
2023-02-22 13:02:52 +0100

    feat: added python extrapolation a2_{0T} chart from C010103

---
+++ frontend/src/utils/steerCalculation/aCoefficients/a20T.ts
@@ -0,0 +1,7 @@
+const a20T = (profileThickness:number,chordRatio:number) => {
+  return (
+(10.81508606*profileThickness+2.73825874)*Math.log((-44.95742571*profileThickness+11.01818345)*chordRatio+(-0.90731198*profileThickness+1.43220827))
+  )
+}
+
+export default a20T
