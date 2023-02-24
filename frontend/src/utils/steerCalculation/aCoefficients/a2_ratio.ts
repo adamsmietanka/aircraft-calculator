bc2f1e7589df3e03ea4a6acb449ffa9a5f161d78
LukaszOgrodnik <ogrud2000@gmail.com>
2023-02-24 13:48:46 +0100

    fix: add condition for a2 calculation exceeding value of 1.
    add console logs to inform the user about exceeding that conditions.

--- frontend/src/utils/steerCalculation/aCoefficients/a2_ratio.ts
+++ frontend/src/utils/steerCalculation/aCoefficients/a2_ratio.ts
@@ -6,15 +6,25 @@ const a2_ratio = (a1Ratio: number, chordRatio: number) => {
     b1: [2.08816745, -1.15933741],
     c1: [2.29586863, -1.32065881],
   };
-  if (a1Ratio >= 1){
-    return 1
+  if (a1Ratio >= 1) {
+    console.log(
+      "Warning:In a2ratio estimation the value of a1ratio exeedds or is equal to 1. The value of the of a2ratio is automatically set to 1."
+    );
+    return 1;
   }
-  return (
+  let a2 =
     polynomynalFunction(a1Ratio, params["a1"]) *
       chordRatio *
       Math.log(polynomynalFunction(a1Ratio, params["b1"]) * chordRatio) +
-    polynomynalFunction(a1Ratio, params["c1"])
-  );
+    polynomynalFunction(a1Ratio, params["c1"]);
+  if (a2 >= 1) {
+    console.log(
+      "Warning:In a2ratio estimation the estimated of a1ratio exeedds or is equal 1. The value of the of a2ratio is automatically set to 1."
+    );
+    return 1;
+  } else {
+    return a2;
+  }
 };
 
 export default a2_ratio;
