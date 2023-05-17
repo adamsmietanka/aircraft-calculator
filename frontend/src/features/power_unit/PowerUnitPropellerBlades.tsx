ef1ffe302b8083262faa5bb6988f2de67e7007a3
adamsmietanka <adamsmietanka@outlook.com>
2023-05-17 17:33:40 +0200

    feat: add propeller blade animation on change

--- frontend/src/features/power_unit/PowerUnitPropellerBlades.tsx
+++ frontend/src/features/power_unit/PowerUnitPropellerBlades.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ReactComponent as Two } from "../../assets/blades/two.svg";
 import { ReactComponent as Three } from "../../assets/blades/three.svg";
 import { ReactComponent as Four } from "../../assets/blades/four.svg";
@@ -7,6 +6,7 @@ import { usePropellerStore } from "./stores/usePropeller";
 const PowerUnitPropellerBlades = () => {
   const blades = usePropellerStore((state) => state.blades);
   const setBlades = usePropellerStore((state) => state.setBlades);
+
   return (
     <div className="form-control">
       <label className="label">
@@ -18,14 +18,26 @@ const PowerUnitPropellerBlades = () => {
           min="2"
           max="4"
           value={blades}
-          className="range"
+          className="range range-xs"
           step="1"
           onChange={(e) => setBlades(parseFloat(e.target.value))}
         />
         <div className="w-full flex justify-between mt-1">
-          <Two className="w-8 h-8 text-color" />
-          <Three className="w-7 h-7 text-color" />
-          <Four className="w-8 h-8 text-color" />
+          <Two
+            className={`w-8 h-8 text-color -ml-2 ${
+              blades === 2 && "animate-spin-slow"
+            }`}
+          />
+          <Three
+            className={`w-8 h-8 text-color ${
+              blades === 3 && "animate-spin-slow"
+            }`}
+          />
+          <Four
+            className={`w-8 h-8 text-color -mr-2 ${
+              blades === 4 && "animate-spin-slow"
+            }`}
+          />
         </div>
       </label>
     </div>
