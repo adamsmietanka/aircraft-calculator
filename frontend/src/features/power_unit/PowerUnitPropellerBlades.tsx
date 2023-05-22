b54b64ca4d0122b2a636ecc0f427822c6bb1d36a
adamsmietanka <adamsmietanka@outlook.com>
2023-05-22 23:00:41 +0200

    feat: add a blocker for propeller spin on render

--- frontend/src/features/power_unit/PowerUnitPropellerBlades.tsx
+++ frontend/src/features/power_unit/PowerUnitPropellerBlades.tsx
@@ -2,10 +2,13 @@ import { ReactComponent as Two } from "../../assets/blades/two.svg";
 import { ReactComponent as Three } from "../../assets/blades/three.svg";
 import { ReactComponent as Four } from "../../assets/blades/four.svg";
 import { usePropellerStore } from "./stores/usePropeller";
+import { useDidMount } from "./hooks/useDidMount";
 
 const PowerUnitPropellerBlades = () => {
   const blades = usePropellerStore((state) => state.blades);
   const setBlades = usePropellerStore((state) => state.setBlades);
+
+  const didMount = useDidMount();
 
   return (
     <div className="form-control">
@@ -25,17 +28,17 @@ const PowerUnitPropellerBlades = () => {
         <div className="w-full flex justify-between mt-1">
           <Two
             className={`w-8 h-8 text-color -ml-2 ${
-              blades === 2 && "animate-spin-slow"
+              didMount() && blades === 2 && "animate-spin-slow"
             }`}
           />
           <Three
             className={`w-8 h-8 text-color ${
-              blades === 3 && "animate-spin-slow"
+              didMount() && blades === 3 && "animate-spin-slow"
             }`}
           />
           <Four
             className={`w-8 h-8 text-color -mr-2 ${
-              blades === 4 && "animate-spin-slow"
+              didMount() && blades === 4 && "animate-spin-slow"
             }`}
           />
         </div>
