433a7740061eb661c3294a851176ba78e856d232
adamsmietanka <adamsmietanka@outlook.com>
2023-06-02 17:26:48 +0200

    feat: move the 3D surfaces

--- frontend/src/features/power_unit/three/PowerUnitResults3D.tsx
+++ frontend/src/features/power_unit/three/PowerUnitResults3D.tsx
@@ -17,22 +17,23 @@ const Lights = () => {
 };
 
 const PowerUnitResults3D = () => {
-  const style = getComputedStyle(document.body);
-  const b1 = style.getPropertyValue("--b1").replaceAll(" ", ",")
-  console.log(b1);
-  const bgColor = new THREE.Color(`hsl(${b1})`);
+  // const style = getComputedStyle(document.body);
+  // const b1 = style.getPropertyValue("--b1").replaceAll(" ", ",");
+  // console.log(b1);
+  // const bgColor = new THREE.Color(`hsl(${b1})`);
   return (
     <div className="h-96 w-full">
       <Canvas orthographic camera={{ zoom: 30, position: [-10, 10, 15] }}>
-        {/* <fog attach="fog" args={[bgColor, 20, 25]} /> */}
-
-        <axesHelper />
         <ambientLight intensity={0.4} />
         <Lights />
-        <SurfaceCp />
-        <SurfaceEff position={[0, 0, 10]} />
+        <SurfaceCp position={[-1, 0, 0]} />
+        <SurfaceEff position={[-1, 0, 10]} />
         <GridLines />
-        <OrbitControls autoRotate autoRotateSpeed={0.25} target={[3.5, 1, 7.5]} />
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={0.25}
+          target={[2.5, 1, 7.5]}
+        />
         {/* <Stats /> */}
       </Canvas>
     </div>
