39168664d68b72919921f913a8cc0f878d831ff5
adamsmietanka <adamsmietanka@outlook.com>
2023-06-15 21:38:16 +0200

    feat: add simple shader for trace control

---
+++ frontend/src/features/power_unit/three/Trace.tsx
@@ -0,0 +1,111 @@
+import { useFrame } from "@react-three/fiber";
+import React, { useMemo, useRef } from "react";
+import * as THREE from "three";
+
+const vertexShader = `
+    uniform float u_time;
+    uniform float u_time_start;
+    uniform float u_duration;
+    attribute vec3 positionFrom;
+    attribute vec3 positionTarget;
+
+    float linear_easing() {
+        float time = u_time - u_time_start;
+        if (time < 0.0) {
+            return 0.0;
+        }
+        if (0.0 < time && time < u_duration) {
+            return time / u_duration;
+        }
+        return 1.0;
+    }
+  
+  void main() {
+    float inter = linear_easing();
+    vec3 new_position = mix(position, positionTarget, inter);
+    vec4 modelPosition = modelMatrix * vec4(new_position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+  
+    gl_Position = projectedPosition;
+  }
+`;
+
+const fragmentShader = `
+
+vec3 colorA = vec3(0.912,0.191,0.652);
+vec3 colorB = vec3(1.000,0.777,0.052);
+
+void main() {
+  vec3 color = mix(colorA, colorB, 1.0);
+
+  gl_FragColor = vec4(color,1.0);
+}
+`;
+
+const Trace = () => {
+  const shaderMaterialRef = useRef<THREE.ShaderMaterial>(null);
+
+  const verts = new Float32Array([0, 0, 0, 2, 0, 5]);
+  const vertsFrom = new Float32Array([0, 0, 0, 2, 0, -2]);
+  const vertsTarget = new Float32Array([0, 0, 0, 2, 0, 2]);
+
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+      u_time_start: {
+        value: 0.0,
+      },
+      u_duration: {
+        value: 2.0,
+      },
+      u_colorA: { value: new THREE.Color("#FFE486") },
+      u_colorB: { value: new THREE.Color("#FEB3D9") },
+    }),
+    []
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    //   console.log(clock.getElapsedTime())
+    if (shaderMaterialRef.current) {
+      shaderMaterialRef.current.uniforms.u_time.value = clock.getElapsedTime();
+    }
+  });
+  return (
+    <lineSegments>
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={verts.length / 3}
+          array={verts}
+          itemSize={3}
+        />
+        <bufferAttribute
+          attach="attributes-positionTarget"
+          count={vertsTarget.length / 3}
+          array={vertsTarget}
+          itemSize={3}
+        />
+        <bufferAttribute
+          attach="attributes-positionFrom"
+          count={vertsFrom.length / 3}
+          array={vertsFrom}
+          itemSize={3}
+        />
+        {/* <bufferAttribute attach="attributes-colors" args={[colors, 3]} /> */}
+      </bufferGeometry>
+      <shaderMaterial
+        ref={shaderMaterialRef}
+        uniforms={uniforms}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
+      {/* <lineBasicMaterial color="red" linewidth={40} /> */}
+    </lineSegments>
+  );
+};
+
+export default Trace;
