83df3649d57ef9e4ebdff1ecceca474cd5c1aafd
adamsmietanka <adamsmietanka@outlook.com>
2023-06-27 17:04:49 +0200

    feat: add themes to 2d charts

--- frontend/src/features/power_unit/three/shaders/line.fragment.glsl
+++ frontend/src/features/power_unit/three/shaders/line.fragment.glsl
@@ -4,15 +4,12 @@ uniform float u_opacity_stagger;
 uniform float u_opacity_delay;
 uniform float u_count;
 varying float v_index;
-
-vec3 colorA = vec3(0.912,0.191,0.652);
-vec3 colorB = vec3(1.000,0.777,0.052);
+uniform vec3 u_color;
 
 float opacity_change_duration = 0.2;
 
 void main() {
-  vec3 color = mix(colorA, colorB, 1.0);
-  gl_FragColor.rgb = color;
+  gl_FragColor.rgb = u_color;
   
   float point_delay = u_opacity_delay + linear(v_index / u_count) * u_opacity_stagger;
   gl_FragColor.a = clamp((u_time - point_delay) / opacity_change_duration, 0.0, 1.0);
