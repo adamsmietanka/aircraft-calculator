3e2aed5b8c0a18c78042d3709447b184940f69dd
adamsmietanka <adamsmietanka@outlook.com>
2023-07-11 22:15:50 +0200

    feat: add automatic grid colors to 2D charts

--- frontend/src/features/power_unit/three/LinesHorizontal.tsx
+++ frontend/src/features/power_unit/three/LinesHorizontal.tsx
@@ -3,7 +3,7 @@ import vertex from "./shaders/line.vertex.glsl";
 import fragment from "./shaders/line.fragment.glsl";
 import useLinesHorizontal from "./hooks/useLinesHorizontal";
 import { Html, Line, Text } from "@react-three/drei";
-import { NUMBERS_PADDING, TITLE_PADDING } from "./config";
+import { NUMBERS_PADDING, TITLE_PADDING, useCSSColors } from "./config";
 import useChartUnits from "../../settings/hooks/useChartUnits";
 import { Axis } from "./Chart2D";
 import { useFrame } from "@react-three/fiber";
@@ -25,20 +25,17 @@ interface AxisProps {
 
 const LinesHorizontal = ({ ticks, axis, scale }: AxisProps) => {
   const AnimatedText = animated(Text);
+  const { gridColor } = useCSSColors();
 
   const markersRef = useSpringRef();
   const titleRef = useSpringRef();
 
-  const [opacityTrail] = useTrail(
-    ticks.length,
-    () => ({
-      ref: markersRef,
-      delay: 500,
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-    }),
-    []
-  );
+  const opacityTrail = useTrail(ticks.length, {
+    ref: markersRef,
+    delay: 500,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
 
   const title = useSpring({
     ref: titleRef,
@@ -67,7 +64,8 @@ const LinesHorizontal = ({ ticks, axis, scale }: AxisProps) => {
       ))}
       <AnimatedText
         position={[-2 * TITLE_PADDING, 8, 0]}
-        fontSize={0.6}
+        fontSize={0.5}
+        color={gridColor}
         fillOpacity={title.opacity}
       >
         {`${axis.name} [${unit}]`}
