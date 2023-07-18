a8c5dc44a64bdc159716f80e5d799283b185abd9
adamsmietanka <adamsmietanka@outlook.com>
2023-07-18 12:08:37 +0200

    feat: add a way to animate Line
    Albeit not working. The geometry doesn't seem to respond to changes and requires React rerenders

--- frontend/src/features/power_unit/three/Trace.tsx
+++ frontend/src/features/power_unit/three/Trace.tsx
@@ -1,14 +1,13 @@
 import { useRef } from "react";
-import {
-  animated,
-  useSpring,
-  config,
-  SpringValue,
-} from "@react-spring/three";
+import { animated, useSpring, config, SpringValue } from "@react-spring/three";
 import { extend, useFrame } from "@react-three/fiber";
 import { MeshLineGeometry, MeshLineMaterial } from "meshline";
 import { Object3DNode, MaterialNode } from "@react-three/fiber";
 import { useCSSColors } from "./config";
+
+import { Line2, LineSegments2 } from "three-stdlib";
+import { useThree } from "@react-three/fiber";
+import { Line } from "@react-three/drei";
 
 declare module "@react-three/fiber" {
   interface ThreeElements {
@@ -26,10 +25,10 @@ interface TraceProps {
 
 const Trace = ({ trace, scale }: TraceProps) => {
   const test = useSpring({
-    delay: 250,
+    delay: 1000,
     from: { opacity: 0 },
     to: { opacity: 1 },
-    config: config.slow,
+    config: config.molasses,
   });
 
   const [marker] = useSpring(
@@ -42,27 +41,60 @@ const Trace = ({ trace, scale }: TraceProps) => {
 
   const geometryRef = useRef<MeshLineGeometry>(null);
   const materialRef = useRef<MeshLineMaterial>(null);
-  
+
   const { traceColor } = useCSSColors();
 
   useFrame(() => {
     const interpolatedPoints = marker.points.get();
     const interpolatedOpacity = test.opacity.get();
+    // console.log(
+    //   interpolatedPoints.slice(
+    //     0,
+    //     Math.ceil(interpolatedOpacity * interpolatedPoints.length)
+    //   )
+    // );
+
+    const numberOfPoints =
+      Math.ceil(interpolatedOpacity * trace.length) * 3 || 6;
+    console.log(numberOfPoints);
 
     if (geometryRef.current && materialRef.current) {
-      materialRef.current.opacity = interpolatedOpacity;
-      geometryRef.current.setPoints(interpolatedPoints);
+      // materialRef.current.opacity = interpolatedOpacity;
+      numberOfPoints > 50 &&
+        geometryRef.current.setPoints(
+          interpolatedPoints.slice(0, numberOfPoints)
+        );
+    }
+
+    if (lineRef.current) {
+      lineRef.current.geometry.setPositions(
+        interpolatedPoints.slice(0, numberOfPoints)
+      );
+      // lineRef.current.material.opacity = interpolatedOpacity;
     }
   });
 
+  const lineRef = useRef<Line2 | LineSegments2>(null);
+
   return (
     <animated.mesh scale={marker.scale}>
-      <meshLineGeometry ref={geometryRef} points={[0, 0, 0]} />
+      {/* <meshLineGeometry ref={geometryRef} points={[0, 0, 0]} />
       <meshLineMaterial
         ref={materialRef}
-        lineWidth={0.02}
+        lineWidth={0.015}
         color={traceColor}
         transparent
+        // wireframe
+      /> */}
+      <Line
+        ref={lineRef}
+        points={[
+          [0, 0],
+          [0, 0],
+        ]}
+        lineWidth={8}
+        // transparent
+        color={traceColor}
       />
     </animated.mesh>
   );
