c93a0f8534b7fea1f6ef2565c1ba842bcad07ee6
adamsmietanka <adamsmietanka@outlook.com>
2023-07-19 16:12:21 +0200

    feat: add propeller power chart

--- frontend/src/features/power_unit/PowerUnitResults.tsx
+++ frontend/src/features/power_unit/PowerUnitResults.tsx
@@ -8,11 +8,21 @@ import { useResultsStore } from "./stores/useResults";
 import InputUnits from "../common/inputs/InputUnits";
 import PowerUnitPropellerDiameter from "./PowerUnitPropellerDiameter";
 import InputNumber from "../common/inputs/InputNumber";
+import { Canvas } from "@react-three/fiber";
+import LineChart from "./three/LineChart";
 
 const PowerUnitResults = () => {
   const altitude = useResultsStore((state) => state.altitude);
   const setAltitude = useResultsStore((state) => state.setAltitude);
   const [power, Cp] = usePowerUnitResults();
+
+  const table = useResultsStore((state) => state.table);
+  const traces = [
+    {
+      name: "power",
+      points: table.map(({ v, prop_power }) => [v, prop_power, 0]),
+    },
+  ];
 
   return (
     <div className="flex w-full h-full p-4">
@@ -40,9 +50,20 @@ const PowerUnitResults = () => {
         <PowerUnitPropellerPitch />
         <PowerUnitPropellerDiameter />
       </div>
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-4 w-3/5">
         <PowerUnitResults3D />
-        <PowerUnitResultsPowerChart />
+        <div className="h-2/5">
+          <Canvas orthographic camera={{ zoom: 30 }}>
+            <LineChart
+              traces={traces}
+              xAxis={{ name: "Speed", type: "speed" }}
+              yAxis={{
+                name: "Power",
+                type: "power",
+              }}
+            />
+          </Canvas>
+        </div>
       </div>
     </div>
   );
