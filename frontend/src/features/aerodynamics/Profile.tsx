6611cf2f51b4ac601fc895a8f3bf738d03669d75
adamsmietanka <adamsmietanka@outlook.com>
2023-08-07 12:23:34 +0200

    refactor(wing): extract profile table

--- frontend/src/features/aerodynamics/Profile.tsx
+++ frontend/src/features/aerodynamics/Profile.tsx
@@ -1,84 +1,11 @@
-import { useEffect, useMemo } from "react";
-import profiles from "./data/profiles_interpolated";
 import { Canvas } from "@react-three/fiber";
 import LineChart from "../power_unit/three/LineChart";
-import { useWingStore } from "./stores/useWing";
 import ProfileVisualizer from "./three/ProfileVisualizer";
-import generate_coefficients from "./utils/generator";
 import useProfileCharts from "./hooks/useProfileCharts";
-
-const ProfileTable = () => {
-  const wing = useWingStore();
-
-  const tableData = useMemo(() => {
-    let table = Object.keys(profiles).map((profile) => {
-      const cz = profiles[profile].cz;
-      const cd = profiles[profile].cd;
-      const highest = cz.reduce((previous, current) => {
-        if (current[1] > previous[1]) {
-          return current;
-        }
-        return previous;
-      });
-      let lowest = [0, 0.9];
-      if (cd.length) {
-        lowest = cd.reduce((previous, current) => {
-          if (current[1] < previous[1]) {
-            return current;
-          }
-          return previous;
-        });
-      }
-      return {
-        name: profile,
-        maxCz: highest[1],
-        angleOfMaxCz: highest[0],
-        minCd: lowest[1],
-        czOfMinCd: lowest[0],
-      };
-    });
-    return table.sort((a, b) => a.name.localeCompare(b.name));
-  }, []);
-  return (
-    <div className="">
-      <table className="table">
-        {/* head */}
-        <thead>
-          <tr>
-            <th>Profile</th>
-            <th>Max Cz</th>
-            <th>Angle</th>
-            <th>Min Cd</th>
-            <th>Cz of Min Cd</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((row) => (
-            <tr
-              className={`${
-                wing.profile === row.name && "bg-base-200"
-              } cursor-pointer`}
-              onClick={() => wing.setProfile(row.name)}
-            >
-              <td>{row.name}</td>
-              <td>{row.maxCz.toFixed(3)}</td>
-              <td>{row.angleOfMaxCz.toFixed(1)}</td>
-              <td>{row.minCd.toFixed(4)}</td>
-              <td>{row.czOfMinCd.toFixed(2)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+import ProfileTable from "./ProfileTable";
 
 const Profile = () => {
   const { points, pointsCd, useProfileChartsStore } = useProfileCharts();
-
-  useEffect(() => {
-    generate_coefficients();
-  }, []);
 
   return (
     <div className="flex space-x-4 h-full mt-4">
