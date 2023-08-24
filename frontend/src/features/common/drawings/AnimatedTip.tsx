213c870c03ea31bdf7eeabb83e22652cdb85accb
adamsmietanka <adamsmietanka@outlook.com>
2023-08-24 15:50:44 +0200

    refactor(wing): extract tip logic from inputs

---
+++ frontend/src/features/common/drawings/AnimatedTip.tsx
@@ -0,0 +1,43 @@
+import { Cone } from "@react-three/drei";
+import { SpringValue, animated } from "@react-spring/three";
+import {
+  MEASUREMENT_DISTANCE,
+  VECTOR_TIP_LENGTH,
+  VECTOR_TIP_WIDTH,
+  useCSSColors,
+} from "../three/config";
+
+interface Props {
+  end?: boolean;
+  scale: SpringValue<number>;
+  value?: SpringValue<number>;
+}
+
+const AnimatedTip = ({
+  end = false,
+  scale,
+  value = new SpringValue(0),
+}: Props) => {
+  const AnimatedCone = animated(Cone);
+  const { gridColor } = useCSSColors();
+
+  return (
+    <animated.mesh
+      rotation-z={end ? Math.PI : 0}
+      position-x={value}
+      position-y={scale.to((scale) => MEASUREMENT_DISTANCE / scale)}
+    >
+      <AnimatedCone
+        args={[VECTOR_TIP_WIDTH, VECTOR_TIP_LENGTH, 32]}
+        position-x={scale.to((scale) => VECTOR_TIP_LENGTH / (2 * scale))}
+        scale={scale.to((s) => 1 / s)}
+        material-transparent
+        rotation-z={Math.PI / 2}
+        material-color={gridColor}
+        material-opacity={1}
+      />
+    </animated.mesh>
+  );
+};
+
+export default AnimatedTip;
