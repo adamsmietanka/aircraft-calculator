c3dcec710f5aed4246c3e8fe800ba1093a102a05
adamsmietanka <adamsmietanka@outlook.com>
2023-09-11 13:26:59 +0200

    fix(profile): make the profile dropdown smaller

--- frontend/src/features/aerodynamics/ProfileChoose.tsx
+++ frontend/src/features/aerodynamics/ProfileChoose.tsx
@@ -26,7 +26,7 @@ const ProfileChoose = () => {
           </label>
           <ul
             tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
           >
             {profileList.map(
               (p) =>
