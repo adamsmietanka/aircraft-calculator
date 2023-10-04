7a501d0cda2037d1cd9f02226324a9f3a32e65c3
adamsmietanka <adamsmietanka@outlook.com>
2023-10-04 13:58:48 +0200

    feat(profile): add categories in table

--- frontend/src/features/aerodynamics/ProfileModal.tsx
+++ frontend/src/features/aerodynamics/ProfileModal.tsx
@@ -3,7 +3,7 @@ import ProfileTable from "./ProfileTable";
 const ProfileModal = () => {
   return (
     <dialog id="profile_modal" className="modal ">
-      <form method="dialog" className="modal-box max-w-3xl max-h-full">
+      <form method="dialog" className="modal-box max-w-4xl max-h-full">
         <h3>Profile Catalog</h3>
         <div className="">
           <ProfileTable />
