8d679dba052c57d1eb5900ba15c8359395fe09f5
adamsmietanka <adamsmietanka@outlook.com>
2023-10-23 19:09:40 +0200

    feat(compass): add a second tower

--- frontend/src/features/compass/NavigationHyperbolic.tsx
+++ frontend/src/features/compass/NavigationHyperbolic.tsx
@@ -12,7 +12,6 @@ interface Props {
 
 const NavigationHyperbolic = ({ opacity }: Props) => {
   const [active, setActive] = useState<THREE.Object3D>(null!);
-  const onTransform = (e: THREE.Event | undefined) => {};
   const [gizmoSpring] = useSpring(
     () => ({
       size: !!active ? 0.6 : 0,
@@ -23,53 +22,93 @@ const NavigationHyperbolic = ({ opacity }: Props) => {
   const AnimatedTransform = animated(TransformControls);
 
   const timedelta = useCompassStore((state) => state.timedelta);
+  const ACdelta = useCompassStore((state) => state.ACdelta);
+  const A = useCompassStore((state) => state.A);
+  const B = useCompassStore((state) => state.B);
+  const C = useCompassStore((state) => state.C);
+  const set = useCompassStore((state) => state.set);
   const setTimedelta = useCompassStore((state) => state.setTimedelta);
+  const setACdelta = useCompassStore((state) => state.setACdelta);
 
-  const circleIntersection = () => {
-    const posX = timedelta / 10;
-    const deltaSize = 0.5;
-    const xInter =
-      (Math.pow(1 + posX + deltaSize, 2) - Math.pow(1 - posX + deltaSize, 2)) /
-      4;
-    const yInter = Math.sqrt(
-      Math.pow(1 - posX + deltaSize, 2) - Math.pow(xInter - 1, 2)
-    );
-    const a = (xInter * xInter - posX * posX) / (yInter * yInter);
-    console.log(xInter, yInter);
-  };
-  circleIntersection();
-  const hyperbolaX = (y: number) => {
-    const posX = timedelta / 10;
-    const deltaSize = 0.5;
-    const xInter =
-      (Math.pow(1 + posX + deltaSize, 2) - Math.pow(1 - posX + deltaSize, 2)) /
-      4;
-    const yInter = Math.sqrt(
-      Math.pow(1 - posX + deltaSize, 2) - Math.pow(xInter - 1, 2)
-    );
-    const a = (xInter * xInter - posX * posX) / (yInter * yInter);
-    return (
-      Math.sign(timedelta) *
-      Math.sqrt((timedelta * timedelta) / 100 + y * y * a)
-    );
+  const onTransform = (e: THREE.Event | undefined) => {
+    if (e && e.target.object) {
+      const { tower } = e.target.object.userData;
+      const { x, y } = e.target.object.position;
+      if (tower === "A") {
+        set({ A: { x, y } });
+      }
+      if (tower === "B") {
+        set({ B: { x, y } });
+      }
+      if (tower === "C") {
+        set({ C: { x, y } });
+      }
+    }
   };
 
-  const circle = Array.from(Array(40).keys()).map((i) => [
-    hyperbolaX((i - 20) / 10),
-    (i - 20) / 10,
+  const circle = Array.from(Array(41).keys()).map((i) => [
+    Math.cos((i / 20) * Math.PI),
+    Math.sin((i / 20) * Math.PI),
     0,
   ]);
+
+  const createHyperbola = (
+    first: Record<string, number>,
+    second: Record<string, number>,
+    delta: number
+  ) => {
+    const distance12 = Math.sqrt(
+      Math.pow(second.x - first.x, 2) + Math.pow(second.y - first.y, 2)
+    );
+    const e = distance12 / delta;
+    const p = (distance12 / 2) * (1 - 1 / (e * e));
+
+    const alpha = (e * p) / (e * e - 1);
+    const beta = alpha * Math.sqrt(e * e - 1);
+
+    const hyperX = (y: number) => {
+      if (1 / e === 0) return 0;
+      return alpha * Math.sqrt(1 + Math.pow(y / beta, 2));
+    };
+    const hyper = Array.from(Array(40).keys()).map((i) => [
+      hyperX((i - 20) / 10) + first.x + distance12 / 2,
+      (i - 20) / 10 + first.y,
+      0,
+    ]);
+    return hyper;
+  };
+  // const deltaRadius = 0.1;
+
+  const hyper = createHyperbola(A, B, (2 * timedelta) / 10);
+  const hyper2 = createHyperbola(A, C, (2 * ACdelta) / 10);
+  const [towerSpring] = useSpring(
+    () => ({
+      rotationAB: Math.atan2(B.y - A.y, B.x - A.x),
+      rotationAC: Math.atan2(C.y - A.y, C.x - A.x),
+      Ax: A.x,
+      Ay: A.y,
+    }),
+    [A, B, C]
+  );
 
   return (
     <>
       <Inputs3D gridPositionX={-1}>
         <InputSlider
-          label="Time delta"
+          label="AB time diff"
           unit="ms"
           value={timedelta}
           min={-9}
           max={9}
           setter={setTimedelta}
+        />
+        <InputSlider
+          label="AC time diff"
+          unit="ms"
+          value={ACdelta}
+          min={-9}
+          max={9}
+          setter={setACdelta}
         />
       </Inputs3D>
       <AnimatedTransform
@@ -78,8 +117,6 @@ const NavigationHyperbolic = ({ opacity }: Props) => {
           (size, stepActive) => size * stepActive
         )}
         showZ={false}
-        showX={active?.userData.isTrailing}
-        showY={active?.userData.isTip}
         onChange={onTransform}
         object={active}
         space="local"
@@ -90,18 +127,69 @@ const NavigationHyperbolic = ({ opacity }: Props) => {
         // rotation-x={-Math.PI / 2}
         scale={3}
       >
-        <animated.mesh position-x={-1}>
-          <Sphere args={[0.15, 32, 32]} />
-        </animated.mesh>
-        <animated.mesh position-x={1}>
-          <Sphere args={[0.15, 32, 32]} />
-        </animated.mesh>
-        <AnimatedLine
-          points={circle}
-          style="airstream"
-          color="grid"
-          opacity={opacity.to((o) => (true ? o * 0.33 : 0))}
+        <Sphere
+          args={[0.15, 32, 32]}
+          userData={{ tower: "A" }}
+          onClick={(e) => setActive(e.object)}
+          position-x={A.x}
         />
+        <Sphere
+          args={[0.15, 32, 32]}
+          userData={{ tower: "B" }}
+          onClick={(e) => setActive(e.object)}
+          position-x={B.x}
+        />
+        <Sphere
+          args={[0.15, 32, 32]}
+          userData={{ tower: "C" }}
+          onClick={(e) => setActive(e.object)}
+          position-x={C.x}
+          position-y={C.y}
+        />
+        <animated.mesh position-x={towerSpring.Ax} position-y={towerSpring.Ay}>
+          <animated.mesh rotation-z={towerSpring.rotationAB}>
+            <animated.mesh
+              position-x={towerSpring.Ax.to((x) => -x)}
+              position-y={towerSpring.Ay.to((y) => -y)}
+            >
+              <AnimatedLine
+                points={hyper}
+                style="airstream"
+                color="grid"
+                opacity={opacity.to((o) => (true ? o * 0.33 : 0))}
+              />
+            </animated.mesh>
+          </animated.mesh>
+          <animated.mesh rotation-z={towerSpring.rotationAC}>
+            <animated.mesh
+              position-x={towerSpring.Ax.to((x) => -x)}
+              position-y={towerSpring.Ay.to((y) => -y)}
+            >
+              <AnimatedLine
+                points={hyper2}
+                style="airstream"
+                color="grid"
+                opacity={opacity.to((o) => (true ? o * 0.33 : 0))}
+              />
+            </animated.mesh>
+          </animated.mesh>
+        </animated.mesh>
+        {/* <animated.mesh position-x={-1} scale={1 + delta12 / 2 + deltaRadius}>
+          <AnimatedLine
+            points={circle}
+            style="thin"
+            color="grid"
+            opacity={opacity.to((o) => (true ? o * 0.33 : 0))}
+          />
+        </animated.mesh>
+        <animated.mesh position-x={1} scale={1 - delta12 / 2 + deltaRadius}>
+          <AnimatedLine
+            points={circle}
+            style="thin"
+            color="grid"
+            opacity={opacity.to((o) => (true ? o * 0.33 : 0))}
+          />
+        </animated.mesh> */}
       </animated.mesh>
     </>
   );
--- frontend/src/features/compass/stores/useCompass.tsx
+++ frontend/src/features/compass/stores/useCompass.tsx
@@ -2,12 +2,22 @@ import { create } from "zustand";
 
 export interface CompassState {
   timedelta: number;
+  ACdelta: number;
+  A: Record<string, number>;
+  B: Record<string, number>;
+  C: Record<string, number>;
   setTimedelta: (value: number) => void;
+  setACdelta: (value: number) => void;
   set: (value: Partial<CompassState>) => void;
 }
 
 export const useCompassStore = create<CompassState>()((set) => ({
   timedelta: 0,
+  ACdelta: 0,
+  A: { x: -1, y: 0 },
+  B: { x: 1, y: 0 },
+  C: { x: 0, y: 1.6 },
   setTimedelta: (value) => set({ timedelta: value }),
+  setACdelta: (value) => set({ ACdelta: value }),
   set: (value) => set(value),
 }));
