113c00fb222ae0b98e9d83b2f97a5b7282fbc428
adamsmietanka <adamsmietanka@outlook.com>
2023-11-10 16:39:21 +0100

    fix: make units not affect angles

---
+++ frontend/src/features/common/inputs/InputDrawingAngle.tsx
@@ -0,0 +1,36 @@
+interface Props {
+  small?: boolean;
+  value: number;
+  setter?: (value: number) => void;
+}
+
+const InputDrawingAngle = ({
+  small = false,
+  value,
+  setter,
+}: Props) => {
+  return (
+    <div className="flex">
+      <input
+        className={`input w-28 text-center bg-transparent focus:outline-none pl-8 z-50 ${
+          small && "input-2sm"
+        }`}
+        type="number"
+        value={value}
+        min={0}
+        max={45}
+        onChange={(e) => setter && setter(parseFloat(e.target.value))}
+      />
+      <div className="absolute top-0 left-0 bottom-0 h-12 ml-8 w-14 flex items-center justify-center text-center z-1">
+        <span className="invisible" id="hiddenValue">
+          {value}
+        </span>
+        <span className="units-value" id="unitsValue">
+          &deg;
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default InputDrawingAngle;
