fef8207ca5650e329d4e327221725f9308937f97
adamsmietanka <adamsmietanka@outlook.com>
2023-11-14 18:35:50 +0100

    fix: add a way to get info of default profile

--- frontend/src/features/aerodynamics/hooks/useProfileInfo.ts
+++ frontend/src/features/aerodynamics/hooks/useProfileInfo.ts
@@ -4,7 +4,9 @@ import { table } from "../data/profiles_interpolated";
 const useProfileInfo = () => {
   const profile = useWingStore((state) => state.profile);
   const reynoldsIndex = useWingStore((state) => state.reynoldsIndex);
-  return table[profile][reynoldsIndex];
+  return table.hasOwnProperty(profile)
+    ? table[profile][reynoldsIndex]
+    : table.default[reynoldsIndex];
 };
 
 export default useProfileInfo;
