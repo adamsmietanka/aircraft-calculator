a053913744d7a7a88f70e00279507ae5f679f1dc
adamsmietanka <adamsmietanka@outlook.com>
2023-12-10 19:32:11 +0100

    refactor(wing): merge wing into one geometry

--- frontend/src/features/aerodynamics/three/hooks/useWingModel.ts
+++ frontend/src/features/aerodynamics/three/hooks/useWingModel.ts
@@ -1,17 +1,21 @@
-import { useMemo } from "react";
-import useProfile from "../../hooks/useProfile";
+import { useEffect, useMemo, useState } from "react";
 import { useWingStore } from "../../stores/useWing";
 import { getXTip } from "./useWingSpring";
-import { BufferAttribute, BufferGeometry, Shape, ShapeGeometry } from "three";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Shape,
+  ShapeGeometry,
+} from "three";
 import useWingOutline from "../../hooks/useWingOutline";
 import { NUMBER_OF_AIRFOIL_POINTS } from "../../../common/three/config";
+import * as BufferGeometryUtils from "three/examples/jsm/utils/BufferGeometryUtils";
 import { useProfileStore } from "../../stores/useProfile";
 
 const PANELS = 2 * NUMBER_OF_AIRFOIL_POINTS + 1;
 
 const useWingModel = (customShape?: number) => {
   const wing = useWingStore();
-  const {pathname} = useLocation();
 
   const profilePoints = useProfileStore((state) => state.profile);
   const { modelPoints } = useWingOutline();
@@ -42,62 +46,61 @@ const useWingModel = (customShape?: number) => {
     return modelPoints;
   };
 
-  const tipGeometry = useMemo(() => {
+  const wingGeometry = useMemo(() => new BufferGeometry(), []);
+
+  const [geom, setGeom] = useState<BufferGeometry>(null!);
+
+  useEffect(() => {
+    const { xOutline, yOutline } = getOutline();
     const shape = new Shape();
-
-    const { xOutline, yOutline } = getOutline();
-    if (profilePoints.length) {
-      const tip = profilePoints.map(([x, y, z]) => [
-        xOutline[2][0] + (xOutline[2][1] - xOutline[2][0]) * x,
-        (xOutline[2][1] - xOutline[2][0]) * y,
-        yOutline[2],
-      ]);
-      shape.moveTo(tip[0][0], tip[0][1]);
-
-      for (let i = 1; i < PANELS; i++) {
-        shape.lineTo(tip[i][0], tip[i][1]);
-      }
-      const geometry = new ShapeGeometry(shape);
-      geometry.translate(0, 0, tip[0][2]);
-      return geometry;
-    }
-  }, [profilePoints]);
-
-  const geometry = useMemo(() => {
-    const geom = new BufferGeometry();
-
-    const { xOutline, yOutline } = getOutline();
+    let merged;
     if (profilePoints.length) {
       let arr = [];
 
-      for (let j = 0; j < yOutline.length - 1; j++) {
+      for (let j = 1; j < yOutline.length; j++) {
         const fuse = profilePoints.map(([x, y, z]) => [
+          xOutline[j - 1][0] + (xOutline[j - 1][1] - xOutline[j - 1][0]) * x,
+          (xOutline[j - 1][1] - xOutline[j - 1][0]) * y,
+          yOutline[j - 1],
+        ]);
+
+        const tip = profilePoints.map(([x, y, z]) => [
           xOutline[j][0] + (xOutline[j][1] - xOutline[j][0]) * x,
           (xOutline[j][1] - xOutline[j][0]) * y,
           yOutline[j],
         ]);
 
-        const tip = profilePoints.map(([x, y, z]) => [
-          xOutline[j + 1][0] + (xOutline[j + 1][1] - xOutline[j + 1][0]) * x,
-          (xOutline[j + 1][1] - xOutline[j + 1][0]) * y,
-          yOutline[j + 1],
-        ]);
-
         for (let i = 0; i < PANELS; i++) {
-          arr.push(...fuse[i]);
-          arr.push(...fuse[i + 1]);
-          arr.push(...tip[i]);
-          arr.push(...fuse[i + 1]);
-          arr.push(...tip[i + 1]);
-          arr.push(...tip[i]);
+          arr.push(...fuse[PANELS - i]);
+          arr.push(...fuse[PANELS - i - 1]);
+          arr.push(...tip[PANELS - i]);
+          arr.push(...fuse[PANELS - i - 1]);
+          arr.push(...tip[PANELS - i - 1]);
+          arr.push(...tip[PANELS - i]);
+        }
+        if (j === yOutline.length - 1) {
+          shape.moveTo(tip[0][0], tip[0][1]);
+
+          for (let i = 1; i < PANELS; i++) {
+            shape.lineTo(tip[i][0], tip[i][1]);
+          }
         }
       }
       const attr = new BufferAttribute(new Float32Array(arr), 3);
-      geom.setAttribute("position", attr);
-      geom.computeVertexNormals();
-      return geom;
+      wingGeometry.setAttribute("position", attr);
+      wingGeometry.computeVertexNormals();
+
+      const tipGeometry = new ShapeGeometry(shape).toNonIndexed();
+      tipGeometry.translate(0, 0, -yOutline[0]);
+      tipGeometry.deleteAttribute("uv");
+
+      merged = BufferGeometryUtils.mergeGeometries([wingGeometry, tipGeometry]);
+      tipGeometry.translate(0, 0, 2 * yOutline[0]);
+      merged = BufferGeometryUtils.mergeGeometries([merged, tipGeometry]);
+      setGeom(merged);
+      console.log("created");
     }
   }, [profilePoints]);
-  return { geometry, tipGeometry };
+  return { geom };
 };
 export default useWingModel;
