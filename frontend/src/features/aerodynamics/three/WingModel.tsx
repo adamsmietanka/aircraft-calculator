8783111fee6bdc6b116373e7f7877e4efa993282
adamsmietanka <adamsmietanka@outlook.com>
2023-12-10 19:32:11 +0100

    refactor(wing): merge wing into one geometry

--- frontend/src/features/aerodynamics/three/WingModel.tsx
+++ frontend/src/features/aerodynamics/three/WingModel.tsx
@@ -1,6 +1,5 @@
 import { SpringValue, animated } from "@react-spring/three";
 import useWingModel from "./hooks/useWingModel";
-import { DoubleSide } from "three";
 
 interface Props {
   opacity: SpringValue<number>;
@@ -8,29 +7,17 @@ interface Props {
 }
 
 const WingModel = ({ opacity, shape }: Props) => {
-  const { geometry, tipGeometry } = useWingModel(shape);
+  const { geom } = useWingModel(shape);
 
   return (
-    <>
-      <mesh geometry={geometry}>
-        <animated.meshStandardMaterial
-          color={"lightgray"}
-          side={DoubleSide}
-          metalness={1}
-          transparent
-          opacity={opacity}
-        />
-      </mesh>
-      <mesh geometry={tipGeometry}>
-        <animated.meshStandardMaterial
-          color={"lightgray"}
-          side={DoubleSide}
-          metalness={1}
-          transparent
-          opacity={opacity}
-        />
-      </mesh>
-    </>
+    <mesh geometry={geom}>
+      <animated.meshStandardMaterial
+        color={"lightgray"}
+        metalness={1}
+        transparent
+        opacity={opacity}
+      />
+    </mesh>
   );
 };
 
