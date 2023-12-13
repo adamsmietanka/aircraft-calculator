a12fc8975358c50fbe0d8a0ca4364c2a42a2bea2
adamsmietanka <adamsmietanka@outlook.com>
2023-12-13 21:57:34 +0100

    fix: add model creation on startup

--- frontend/src/features/aerodynamics/three/hooks/useWingModel.ts
+++ frontend/src/features/aerodynamics/three/hooks/useWingModel.ts
@@ -46,60 +46,65 @@ const useWingModel = (customShape?: number) => {
   const wingGeometry = useMemo(() => new BufferGeometry(), []);
 
   const [geom, setGeom] = useState<BufferGeometry>(null!);
+  const { xOutline, yOutline } = getOutline();
+  const shape = new Shape();
 
-  useEffect(() => {
-    const { xOutline, yOutline } = getOutline();
-    const shape = new Shape();
+  const createModel = () => {
+    let arr = [];
     let merged;
-    if (
-      profilePoints.length &&
-      (pathname === "/aerodynamics/fuselage" || pathname === "/")
-    ) {
-      let arr = [];
 
-      for (let j = 1; j < yOutline.length; j++) {
-        const fuse = profilePoints.map(([x, y, z]) => [
-          xOutline[j - 1][0] + (xOutline[j - 1][1] - xOutline[j - 1][0]) * x,
-          (xOutline[j - 1][1] - xOutline[j - 1][0]) * y,
-          yOutline[j - 1],
-        ]);
+    for (let j = 1; j < yOutline.length; j++) {
+      const fuse = profilePoints.map(([x, y, z]) => [
+        xOutline[j - 1][0] + (xOutline[j - 1][1] - xOutline[j - 1][0]) * x,
+        (xOutline[j - 1][1] - xOutline[j - 1][0]) * y,
+        yOutline[j - 1],
+      ]);
 
-        const tip = profilePoints.map(([x, y, z]) => [
-          xOutline[j][0] + (xOutline[j][1] - xOutline[j][0]) * x,
-          (xOutline[j][1] - xOutline[j][0]) * y,
-          yOutline[j],
-        ]);
+      const tip = profilePoints.map(([x, y, z]) => [
+        xOutline[j][0] + (xOutline[j][1] - xOutline[j][0]) * x,
+        (xOutline[j][1] - xOutline[j][0]) * y,
+        yOutline[j],
+      ]);
 
-        for (let i = 0; i < PANELS; i++) {
-          arr.push(...fuse[PANELS - i]);
-          arr.push(...fuse[PANELS - i - 1]);
-          arr.push(...tip[PANELS - i]);
-          arr.push(...fuse[PANELS - i - 1]);
-          arr.push(...tip[PANELS - i - 1]);
-          arr.push(...tip[PANELS - i]);
-        }
-        if (j === yOutline.length - 1) {
-          shape.moveTo(tip[0][0], tip[0][1]);
+      for (let i = 0; i < PANELS; i++) {
+        arr.push(...fuse[PANELS - i]);
+        arr.push(...fuse[PANELS - i - 1]);
+        arr.push(...tip[PANELS - i]);
+        arr.push(...fuse[PANELS - i - 1]);
+        arr.push(...tip[PANELS - i - 1]);
+        arr.push(...tip[PANELS - i]);
+      }
+      if (j === yOutline.length - 1) {
+        shape.moveTo(tip[0][0], tip[0][1]);
 
-          for (let i = 1; i < PANELS; i++) {
-            shape.lineTo(tip[i][0], tip[i][1]);
-          }
+        for (let i = 1; i < PANELS; i++) {
+          shape.lineTo(tip[i][0], tip[i][1]);
         }
       }
-      const attr = new BufferAttribute(new Float32Array(arr), 3);
-      wingGeometry.setAttribute("position", attr);
-      wingGeometry.computeVertexNormals();
-
-      const tipGeometry = new ShapeGeometry(shape).toNonIndexed();
-      tipGeometry.translate(0, 0, -yOutline[0]);
-      tipGeometry.deleteAttribute("uv");
-
-      merged = BufferGeometryUtils.mergeGeometries([wingGeometry, tipGeometry]);
-      tipGeometry.translate(0, 0, 2 * yOutline[0]);
-      merged = BufferGeometryUtils.mergeGeometries([merged, tipGeometry]);
-      setGeom(merged);
     }
+    const attr = new BufferAttribute(new Float32Array(arr), 3);
+    wingGeometry.setAttribute("position", attr);
+    wingGeometry.computeVertexNormals();
+
+    const tipGeometry = new ShapeGeometry(shape).toNonIndexed();
+    tipGeometry.translate(0, 0, -yOutline[0]);
+    tipGeometry.deleteAttribute("uv");
+
+    merged = BufferGeometryUtils.mergeGeometries([wingGeometry, tipGeometry]);
+    tipGeometry.translate(0, 0, 2 * yOutline[0]);
+    merged = BufferGeometryUtils.mergeGeometries([merged, tipGeometry]);
+    setGeom(merged);
+  };
+
+  useEffect(() => {
+    profilePoints.length &&
+      (pathname === "/aerodynamics/fuselage" || pathname === "/") &&
+      createModel();
   }, [profilePoints, pathname]);
+
+  useEffect(() => {
+    createModel();
+  }, []);
   return { geom };
 };
 export default useWingModel;
