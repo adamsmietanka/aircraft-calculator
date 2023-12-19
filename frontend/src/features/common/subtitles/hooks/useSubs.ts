0c1d1abcfc8d58816e7c04e18d46150a7bfa7498
adamsmietanka <adamsmietanka@outlook.com>
2023-12-19 03:49:27 +0100

    refactor(intro): switch to the new animation system

--- frontend/src/features/common/subtitles/hooks/useSubs.ts
+++ frontend/src/features/common/subtitles/hooks/useSubs.ts
@@ -6,7 +6,6 @@ const useSubs = () => {
   const setSub = useSubtitleStore((state) => state.setSub);
   const show = useSubtitleStore((state) => state.show);
   const hide = useSubtitleStore((state) => state.hide);
-  const inAnimation = useSubtitleStore((state) => state.inAnimation);
   const setInAnimation = useSubtitleStore((state) => state.setInAnimation);
 
   const presentation = useNavigationStore((state) => state.presentation);
@@ -35,30 +34,40 @@ const useSubs = () => {
 
   const subLength = (text: string | JSX.Element) => {
     if (typeof text === "string") return text.length;
+    const { children } = text.props;
+    if (typeof children === "string") return children.length;
     return text.props.children.reduce(
       (acc: number, val: string | JSX.Element) =>
         typeof val === "string"
           ? acc + val.length
-          : acc + val.props.tex.length / 2,
+          : acc + (val.props.tex?.length / 2 || val.props.children?.length),
       0
     );
   };
 
-  const subtitle = async (text: string | JSX.Element, duration = 3000) => {
-    console.log(inAnimation);
+  const prolongShorter = (scale: number) =>
+    scale < 1 ? Math.pow(scale, 0.4) : scale;
+
+  const subtitle = async (
+    text: string | JSX.Element,
+    sideEffect = () => {},
+    duration = 3000
+  ) => {
     if (!useSubtitleStore.getState().inAnimation) throw Error();
-    duration *= subLength(text) / 80;
+    duration *= prolongShorter(subLength(text) / 60);
     if (!presentation) {
       setSub(text);
       show();
+      await sideEffect();
       await waitForClick(duration);
       hide();
-      await waitForClick(1000);
+      await waitForClick(750);
       if (!useSubtitleStore.getState().inAnimation) throw Error();
     } else {
       setSub(text);
       await waitForClick(0);
       show();
+      await sideEffect();
       await waitForClick(0);
       hide();
     }
