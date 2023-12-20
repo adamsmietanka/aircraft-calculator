c63cf2daee376ea24bba4d3bff6db4cbde31753b
adamsmietanka <adamsmietanka@outlook.com>
2023-12-20 01:50:12 +0100

    refactor(inducedDrag): synchronize subtitles

--- frontend/src/features/aerodynamics/three/tutorials/InducedDrag.tsx
+++ frontend/src/features/aerodynamics/three/tutorials/InducedDrag.tsx
@@ -1,6 +1,5 @@
-import { useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { animated, useSpring } from "@react-spring/three";
+import { useState } from "react";
+import { animated } from "@react-spring/three";
 import { useHoverProfileStore } from "../../stores/useHoverProfile";
 import { useWingStore } from "../../stores/useWing";
 import useProfileCharts from "../../hooks/useProfileCharts";
@@ -11,7 +10,6 @@ import SpeedSlider from "./SpeedSlider";
 import useWingScale from "../../hooks/useWingScale";
 import useProfileVisualizer from "../hooks/useProfileVisualizer";
 import Formula from "../../../common/Formula";
-import { useSubtitleStore } from "../../../common/subtitles/stores/useSubtitles";
 import useSubs from "../../../common/subtitles/hooks/useSubs";
 import InducedDragTunnel from "./InducedDragTunnel";
 import { useInducedDragStore } from "./stores/useInducedDrag";
@@ -43,15 +41,19 @@ const InducedDrag = ({ opacity, visible }: ElementProps) => {
 
   const animation = async () => {
     await sub("We have studied the aerodynamics of a 2D airfoil");
-    setCamera({ center: [-5, 0, 0], spherical: [20, 80, -80] });
-    await wait(500);
-    setInduced({ wing: true });
-    await sub("Which happen to be the same for a wing with an infinite span");
-    set({ showWeight: true });
-    setInduced({ wingspan: 0.5 });
-    await wait(500);
-    setInduced({ tunnel: true });
-    await sub("Or one inside of a wind tunnel");
+    await sub(
+      "Which happen to be the same for a wing with an infinite span",
+      async () => {
+        setCamera({ center: [-5, 0, 0], spherical: [20, 80, -80] });
+        await wait(500);
+        setInduced({ wing: true });
+      }
+    );
+    await sub("Or one inside of a wind tunnel", async () => {
+      setInduced({ wingspan: 0.5 });
+      await wait(500);
+      setInduced({ tunnel: true });
+    });
     setInduced({ tunnel: false });
     await wait(500);
 
@@ -59,72 +61,81 @@ const InducedDrag = ({ opacity, visible }: ElementProps) => {
     setInduced({ wingspan: 1 });
     await wait(500);
     await sub(
-      "Airflow speeds up along the upper surface creating an area of low pressure",
+      "Airflow creates an area of low pressure on the upper surface",
       () => {
         setInduced({ airstreamOpacity: 2, signs: true });
       }
     );
+    await sub("And a high pressure area at the bottom");
     await sub(
-      "This creates a flow from the lower wing surface to the upper around the wingtip",
+      "This results in a flow from the lower surface to the upper around the wingtip",
       () => {
         setInduced({ span: true, moveSigns: true });
       }
     );
-    setInduced({ span: false, signs: false, airstreamOpacity: 0 });
-    await wait(500);
-    setInduced({ vortex: true });
     await sub(
-      "This combined with the speed of the freeflow creates a vortex at the wingtip"
+      "This combined with the speed of the freeflow creates a vortex at the wingtip",
+      async () => {
+        setInduced({
+          span: false,
+          signs: false,
+          airstreamOpacity: 0,
+          vortex: true,
+        });
+      }
     );
-    setCamera({ spherical: [20, 70, 40] });
-    await wait(500);
-    setMass(1);
     await sub(
-      "When we increase the angle of attack the vortex gets more violent"
+      "When we increase the angle of attack the vortex gets more violent",
+      () => setMass(1)
     );
     setMass(0.5);
     await wait(500);
-    setReynolds(1.25 * 6);
-    await wait(500);
-    await sub("Increasing speed makes the vortex smaller");
+    await sub("Increasing speed makes the vortex smaller", () =>
+      setReynolds(1.5 * 6)
+    );
     setReynolds(1 * 6);
-    setCamera({ center: [-5, -1, 7.5], spherical: [20, 90, 90] });
+    await wait(500);
     await sub(
-      "The vortex deflects the airflow behind the trailing edge downwards"
+      "The vortex deflects the airflow behind the trailing edge downwards",
+      () => setCamera({ center: [-5, -1, 7.5], spherical: [20, 90, 90] })
     );
     await sub("This is called downwash");
-    setCamera({ center: [-5, 0, 0], spherical: [20, 90, 0] });
     await wait(500);
-    setInduced({ vortex: false });
-    await wait(500);
-    setInduced({ lift: true, direction: true });
-    await sub("In a 2D world lift is always vertical");
-    setInduced({ velocities: true });
-    await sub("An airfoil produces no downwash");
-    setInduced({ isWing: true });
-    setInduced({ vortex: true });
-    await sub("The downwash angles the relative airflow backwards");
+    await sub("In a 2D world lift is always vertical", async () => {
+      setCamera({ center: [-5, 0, 0], spherical: [20, 90, 0] });
+      await wait(500);
+      setInduced({ vortex: false });
+      await wait(500);
+      setInduced({ lift: true, direction: true });
+    });
+    await sub("An airfoil produces no downwash", () =>
+      setInduced({ velocities: true })
+    );
+    await sub("Downwash angles the relative airflow backwards", () =>
+      setInduced({ isWing: true, vortex: true })
+    );
     await sub("Lift is always perpendicular to the airflow");
-    setInduced({ drag: true });
+
     await sub(
       <p className="flex">
         The x component of
-        <Formula className="text-primary mt-1" tex="\: L \:" /> is called &nbsp;
-        <p className="text-error">induced drag</p>
-      </p>
+        <span className="text-primary mx-1">lift</span> is called
+        <span className="text-error mx-1">induced drag</span>
+      </p>,
+      () => setInduced({ drag: true })
     );
     await sub(
       <p className="flex">
         It's inveresely proportional to <Formula tex="\: V^2" />
       </p>
     );
-    setInduced({ effLift: true });
     await sub(
       <p className="flex">
         The y component of
-        <Formula className="text-primary mt-1" tex="\: L \:" /> is the &nbsp;
-        <p className="text-secondary">effective lift</p>
-      </p>
+        <span className="text-primary mx-1">lift</span> is the
+        <span className="text-secondary mx-1">effective lift</span>
+      </p>,
+      () => setInduced({ effLift: true })
     );
     await sub("Due to the downwash it's slightly smaller");
     await sub(
@@ -140,6 +151,8 @@ const InducedDrag = ({ opacity, visible }: ElementProps) => {
       wing: false,
       wingspan: 10,
       span: false,
+      signs: false,
+      moveSigns: false,
       airstreamOpacity: 0,
       vortex: false,
       isWing: false,
@@ -156,7 +169,7 @@ const InducedDrag = ({ opacity, visible }: ElementProps) => {
     animation,
     cleanup,
     () => {
-      set({ showVectors: false });
+      set({ showVectors: false, showWeight: true });
       setChart({ hover: true, locked: "Coefficient of Drag" });
     },
     visible
