1bf0b72e63d5987a144e1b323d9b939c7925a192
adamsmietanka <adamsmietanka@outlook.com>
2024-01-06 02:03:47 +0100

    feat(fuse): add animation on section change

--- frontend/src/features/aerodynamics/stores/usePlane.ts
+++ frontend/src/features/aerodynamics/stores/usePlane.ts
@@ -3,6 +3,7 @@ import { create } from "zustand";
 export interface PlaneState {
   configuration: number;
   fuselage: number;
+  fuseLatch: boolean;
   setConfiguration: (value: number) => void;
   setFuselage: (value: number) => void;
   set: (value: Partial<PlaneState>) => void;
@@ -10,8 +11,10 @@ export interface PlaneState {
 
 export const usePlaneStore = create<PlaneState>()((set) => ({
   configuration: 0,
-  fuselage: 2303,
+  fuselage: 2304,
+  fuseLatch: false,
   setConfiguration: (value) => set({ configuration: value }),
-  setFuselage: (value) => set({ fuselage: value }),
+  setFuselage: (value) =>
+    set((state) => ({ fuselage: value, fuseLatch: !state.fuseLatch })),
   set: (value) => set(value),
 }));
--- frontend/src/features/aerodynamics/three/FuseModel.tsx
+++ frontend/src/features/aerodynamics/three/FuseModel.tsx
@@ -1,31 +1,72 @@
-import { SpringValue, animated } from "@react-spring/three";
+import { SpringValue, animated, useSpring } from "@react-spring/three";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { usePlaneStore } from "../stores/usePlane";
+import { useEffect, useRef } from "react";
+import { Mesh } from "three";
 
 interface Props {
   opacity: SpringValue<number>;
 }
 
 const FuseModel = ({ opacity }: Props) => {
-  const { nodes } = useLoader(GLTFLoader, "/models/2303.glb");
+  const { nodes } = useLoader(GLTFLoader, "/models/fuse.glb");
+  const fuselage = usePlaneStore((state) => state.fuselage);
+  const fuseLatch = usePlaneStore((state) => state.fuseLatch);
+
+  const first = useRef<Mesh>(null!);
+  const second = useRef<Mesh>(null!);
+
+  const [spring] = useSpring(
+    () => ({
+      scaleFirst: fuseLatch ? 8 : 1,
+      scaleSecond: fuseLatch ? 1 : 8,
+    }),
+    [fuselage, fuseLatch]
+  );
+
+  const setFuselages = () => {
+    if (fuseLatch) first.current.geometry = nodes[fuselage]?.geometry;
+    else second.current.geometry = nodes[fuselage]?.geometry;
+  };
+
+  useEffect(() => {
+    setFuselages();
+  }, [fuseLatch]);
+
+  useEffect(() => {
+    setFuselages();
+  }, []);
 
   return (
-    <mesh
-      geometry={nodes.fuselage?.geometry}
-      rotation-x={Math.PI / 2}
-      rotation-z={-Math.PI / 2}
-      scale={0.1 * 6}
-      scale-y={0.1 * 8}
-      position-x={-1.5}
-    >
-      <animated.meshStandardMaterial
-        color={"white"}
-        // side={DoubleSide}
-        metalness={0.5}
-        transparent
-        opacity={opacity}
-      />
-    </mesh>
+    <>
+      <animated.mesh
+        ref={first}
+        geometry={nodes[2302]?.geometry}
+        scale={spring.scaleFirst}
+        position-x={-1.5}
+      >
+        <animated.meshStandardMaterial
+          color={"white"}
+          metalness={0.5}
+          transparent
+          opacity={opacity}
+        />
+      </animated.mesh>
+      <animated.mesh
+        ref={second}
+        geometry={nodes[2304]?.geometry}
+        scale={spring.scaleSecond}
+        position-x={-1.5}
+      >
+        <animated.meshStandardMaterial
+          color={"white"}
+          metalness={0.5}
+          transparent
+          opacity={opacity}
+        />
+      </animated.mesh>
+    </>
   );
 };
 
