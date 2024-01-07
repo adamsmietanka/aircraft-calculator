baab13e469be80910b916fb24deda6ed631873e9
adamsmietanka <adamsmietanka@outlook.com>
2024-01-07 19:21:05 +0100

    feat(fuse): add a cd indicator

---
+++ frontend/src/features/aerodynamics/data/fuselages.ts
@@ -0,0 +1,8 @@
+const fuselages: Record<number, { cd: number; area: number }> = {
+  2301: { cd: 0.353, area: 0.0153 },
+  2302: { cd: 0.266, area: 0.0147 },
+  2303: { cd: 0.116, area: 0.0184 },
+  2304: { cd: 0.071, area: 0.0167 },
+};
+
+export default fuselages;
