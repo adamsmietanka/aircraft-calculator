9fd79743cc29bf48444c387541fab94be212b616
adamsmietanka <adamsmietanka@outlook.com>
2024-01-21 18:46:58 +0100

    feat(aero): position vertical stabilizer based on model

--- frontend/src/features/aerodynamics/data/fuselages.ts
+++ frontend/src/features/aerodynamics/data/fuselages.ts
@@ -1,8 +1,11 @@
-const fuselages: Record<number, { cd: number; area: number }> = {
-  2301: { cd: 0.353, area: 0.0153 },
-  2302: { cd: 0.266, area: 0.0147 },
-  2303: { cd: 0.116, area: 0.0184 },
-  2304: { cd: 0.071, area: 0.0167 },
+const fuselages: Record<
+  number,
+  { cd: number; area: number; verticalY: number }
+> = {
+  2301: { cd: 0.353, area: 0.0153, verticalY: 0.084109 },
+  2302: { cd: 0.266, area: 0.0147, verticalY: 0.075217 },
+  2303: { cd: 0.116, area: 0.0184, verticalY: 0.082592 },
+  2304: { cd: 0.071, area: 0.0167, verticalY: 0.012647 },
 };
 
 export default fuselages;
