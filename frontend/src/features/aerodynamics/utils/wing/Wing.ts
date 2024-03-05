2810e09b85929bcdaf56d3238cfd6b791cde837a
adamsmietanka <adamsmietanka@outlook.com>
2024-03-05 17:00:09 +0100

    feat(wing): add flap end panels

--- frontend/src/features/aerodynamics/utils/wing/Wing.ts
+++ frontend/src/features/aerodynamics/utils/wing/Wing.ts
@@ -92,6 +92,7 @@ export class Wing {
       }
     });
   }
+
   createModel = () => {
     const wingGeometry = new BufferGeometry();
     const tipShape = new Shape();
@@ -126,44 +127,50 @@ export class Wing {
 
     let geom = BufferGeometryUtils.mergeGeometries([wingGeometry, tipGeometry]);
 
-    tipGeometry.translate(0, 0, ((this.FLAP_START - 1) * this.span) / 2);
-    geom = BufferGeometryUtils.mergeGeometries([geom, tipGeometry]);
-
     return geom;
   };
 
+  createPanel(points: number[][], y: number) {
+    const shape = new Shape();
+    points = this.profile.transform(points, this.getLE(y), y, this.getChord(y));
+
+    shape.moveTo(points[0][0], points[0][1]);
+
+    for (let i = 1; i < points.length - 1; i++) {
+      shape.lineTo(points[i][0], points[i][1]);
+    }
+    const geom = new ShapeGeometry(shape).toNonIndexed();
+    geom.translate(0, 0, y);
+    geom.deleteAttribute("uv");
+    return geom;
+  }
+
   createFlap = () => {
     const wingGeometry = new BufferGeometry();
-    const tipShape = new Shape();
     this.vertices = [];
 
-    this.createSection(
-      this.profile.getOutlineFlap(1 - this.FLAP_CHORD),
-      this.FLAP_START + 0.01,
-      this.FLAP_END - 0.01
-    );
-
-    const tip = this.profile.transform(
-      this.profile.points,
-      this.getLE(this.span / 2),
-      this.span / 2,
-      this.getChord(this.span / 2)
-    );
-    tipShape.moveTo(tip[0][0], tip[0][1]);
-
-    for (let i = 1; i < PANELS; i++) {
-      tipShape.lineTo(tip[i][0], tip[i][1]);
-    }
+    const points = this.profile.getOutlineFlap(1 - this.FLAP_CHORD);
+    this.createSection(points, this.FLAP_START + 0.01, this.FLAP_END - 0.01);
 
     const attr = new BufferAttribute(new Float32Array(this.vertices), 3);
     wingGeometry.setAttribute("position", attr);
     wingGeometry.computeVertexNormals();
 
-    const tipGeometry = new ShapeGeometry(tipShape).toNonIndexed();
-    tipGeometry.translate(0, 0, this.span / 2);
-    tipGeometry.deleteAttribute("uv");
+    const flapTip = this.createPanel(
+      points,
+      ((this.FLAP_END - 0.01) * this.span) / 2
+    );
 
-    let geom = BufferGeometryUtils.mergeGeometries([wingGeometry, tipGeometry]);
+    const flapLowerTip = this.createPanel(
+      this.profile.points,
+      (this.FLAP_START * this.span) / 2
+    );
+
+    let geom = BufferGeometryUtils.mergeGeometries([
+      wingGeometry,
+      flapTip,
+      flapLowerTip,
+    ]);
 
     return geom;
   };
