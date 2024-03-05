3057acf5889b9e8fa1d8d9c879b637a39d07124e
adamsmietanka <adamsmietanka@outlook.com>
2024-03-05 14:34:05 +0100

    feat(wing): add wing model without flap

--- frontend/src/features/aerodynamics/utils/wing/ProfileFactory.ts
+++ frontend/src/features/aerodynamics/utils/wing/ProfileFactory.ts
@@ -1,9 +1,9 @@
-import { ProfilePoints } from "./Profile";
+import { Profile } from "./Profile";
 import { Profile4Series } from "./Profile4Series";
 import { ProfileFlat } from "./ProfileFlat";
 
 export class ProfileFactory {
-  public static createProfile(name: string): ProfilePoints {
+  public static create(name: string): Profile {
     if (name.length == 2) {
       return new ProfileFlat(name);
     } else {
---
+++ frontend/src/features/aerodynamics/utils/wing/Wing.spec.ts
@@ -0,0 +1,27 @@
+import { Wing } from "./Wing";
+
+jest.mock("three/examples/jsm/utils/BufferGeometryUtils", () => {});
+
+describe("Wing creation", () => {
+  it("creates section points", () => {
+    const config = {
+      span: 2,
+      chord: 1,
+      chordTip: 1,
+      angle: 0,
+      shape: 0,
+    };
+
+    const wing = new Wing(config, "30");
+
+    expect(wing.sectionPoints(0, 0.4)).toEqual([
+      0, 0.1, 0.2, 0.30000000000000004, 0.4,
+    ]);
+
+    expect(wing.sectionPoints(0.4, 0.9)).toEqual([
+      0.4, 0.5, 0.6000000000000001, 0.7000000000000001, 0.8, 0.9,
+    ]);
+
+    expect(wing.sectionPoints(0.95, 1)).toEqual([0.95, 1]);
+  });
+});
