3eeb16643016b714d680a3b2cada3f9dda380443
adamsmietanka <adamsmietanka@outlook.com>
2024-03-05 16:13:25 +0100

    feat(wing): add flap model

--- frontend/src/features/aerodynamics/utils/wing/Profile.ts
+++ frontend/src/features/aerodynamics/utils/wing/Profile.ts
@@ -1,3 +1,5 @@
+import { CubicBezierCurve3, CurvePath, Vector3 } from "three";
+
 export interface ProfileDetails {
   M: number;
   P: number;
@@ -15,6 +17,8 @@ export interface ProfilePoints {
 
 export interface ProfileMethods {
   getOutlineWithoutFlap: (X: number) => number[][];
+  getOutlineFlap: (X: number) => number[][];
+  getFlapLE: (X: number) => number[][];
   transform: (
     points: number[][],
     X: number,
@@ -30,22 +34,46 @@ export abstract class Profile implements ProfilePoints, ProfileMethods {
   public camber: number[][];
   public max: number[][];
 
+  public static FLAP_LE_SEGMENTS = 8;
+
   public abstract parseName(name: string): ProfileDetails;
   public abstract createPoints({ M, P, T, F }: ProfileDetails): ProfilePoints;
 
   public getOutlineWithoutFlap(X = 0.6) {
     return [
-      ...this.upper.filter(([x]) => x < X),
-      ...this.lower.filter(([x]) => x < X).toReversed(),
+      ...this.upper.filter(([x]) => x <= X),
+      ...this.lower.filter(([x]) => x <= X).toReversed(),
     ];
   }
 
-  public transform(
-    points: number[][],
-    X: number,
-    Y: number,
-    chord: number
-  ) {
+  public getFlapLE(X: number) {
+    const firstL = this.lower.filter(([x]) => x >= X)[0];
+    const firstU = this.upper.filter(([x]) => x >= X)[0];
+    const center = [
+      (firstU[0] + firstL[0]) / 2,
+      (firstU[1] + firstL[1]) / 2,
+      firstU[2] - firstL[2],
+    ];
+    const radius = (firstU[1] - firstL[1]) / 2;
+    console.log(center);
+    return [...Array(Profile.FLAP_LE_SEGMENTS - 1).keys()].map((i) => [
+      center[0] -
+        radius * Math.sin(((i + 1) / Profile.FLAP_LE_SEGMENTS) * Math.PI),
+      center[1] -
+        radius * Math.cos(((i + 1) / Profile.FLAP_LE_SEGMENTS) * Math.PI),
+      center[2],
+    ]);
+  }
+
+  public getOutlineFlap(X = 0.6) {
+    return [
+      ...this.lower.filter(([x]) => x >= X).toReversed(),
+      ...this.getFlapLE(X),
+      ...this.upper.filter(([x]) => x >= X),
+    ];
+  }
+
+  public transform(points: number[][], X: number, Y: number, chord: number) {
     return points.map(([x, y, z]) => [X + chord * x, chord * y, Y]);
   }
 
