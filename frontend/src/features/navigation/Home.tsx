a59895510b561b9eaf919341da2c03a573042e84
adamsmietanka <adamsmietanka@outlook.com>
2024-03-17 17:20:59 +0100

    feat: update positions

--- frontend/src/features/navigation/Home.tsx
+++ frontend/src/features/navigation/Home.tsx
@@ -20,29 +20,55 @@ import Arrow from "../../assets/arrow.svg?react";
 import { useCameraStore } from "../common/three/stores/useCamera";
 import AnimatedLine from "../common/three/AnimatedLine";
 
-const SPAR_DIAMETER = 0.03;
-
 const transform = (point: number[], { x = 0, y = 0, z = 0 }) => [
   point[0] + x,
   point[1] + y,
   point[2] + z,
 ];
 
-const bearing = { old: [0.35, 0, 0], new: [1, 0, 0] };
-const tank = { old: [0, 0, 0.6], new: [-1, 0, 0] };
-const filter1 = { old: [-0.25, -0.5, 0.1], new: [1, 0, 0.6] };
-const filter2 = { old: [0.25, -0.5, 0.1], new: [1.5, 0, 0.6] };
+const override = (
+  point: number[],
+  { x, y, z }: { x?: number; y?: number; z?: number }
+) => [x ?? point[0], y ?? point[1], z ?? point[2]];
 
-const points = [bearing.old, tank.old, filter1.old, bearing.old];
+const crank = { old: [0, 0, 0], new: [1.35, 0, -0.5] };
+const bearing = { old: [-0.35, 0, 0], new: [1, 0, -0.5] };
+const bearing2 = { old: [0.35, 0, 0], new: [1.7, 0, -0.5] };
+const tank = { old: [0, 0, 0.6], new: [-1, 0, 0] };
+const filter1 = { old: [-0.25, -0.5, 0.1], new: [0, 0, 0.8] };
+const filter2 = { old: [0.25, -0.5, 0.1], new: [0.5, 0, 0.8] };
+
+const b1 = transform(bearing.new, { z: -0.5 });
+const b2 = transform(bearing2.new, { z: -0.5 });
+const t1 = transform(tank.new, { z: 0.5 });
+const t2 = transform(tank.new, { z: -0.5 });
+const f1 = transform(filter1.new, { z: 0.75 });
+const f2 = transform(filter2.new, { z: 0.5 });
+
 const pointsDiagram = [
   bearing.new,
-  transform(bearing.new, { z: -0.5 }),
-  transform(tank.new, { z: -0.5 }),
+  b1,
+  override(b1, { x: tank.new[0] - 0.35 }),
+  transform(t1, { x: -0.35 }),
+  t1,
   tank.new,
-  transform(tank.new, { z: 1 }),
-  transform(filter1.new, { z: 0.4 }),
+  t2,
+  override(t2, { x: filter1.new[0] }),
   filter1.new,
+  f1,
+  override(f1, { x: bearing.new[0] }),
   bearing.new,
+];
+
+const pointsDiagram2 = [
+  override(t1, { x: filter1.new[0] }),
+  override(t1, { x: filter2.new[0] }),
+  filter2.new,
+  f2,
+  override(f2, { x: bearing2.new[0] }),
+  bearing2.new,
+  b2,
+  b1,
 ];
 
 const Home = () => {
@@ -60,8 +86,11 @@ const Home = () => {
   const [spring] = useSpring(
     () => ({
       all: diagram ? 6 : 0,
+      crank: diagram ? crank.new : crank.old,
       bearing: diagram ? bearing.new : bearing.old,
+      bearing2: diagram ? bearing2.new : bearing2.old,
       tank: diagram ? tank.new : tank.old,
+      tankRotation: diagram ? Math.PI / 2 : 0,
       filter1: diagram ? filter1.new : filter1.old,
       filter2: diagram ? filter2.new : filter2.old,
       config: {
@@ -156,18 +185,27 @@ const Home = () => {
               backfaceStroke={"#e3e3e3"} // Color of the lines that are facing away from the camera
             />
           </mesh>
-          <animated.mesh rotation-z={Math.PI / 2}>
-            <cylinderGeometry args={[0.07, 0.07, 1, 32]} />
-            <meshPhongMaterial />
-          </animated.mesh>
           <animated.mesh position-x={spring.all}>
+            <animated.mesh rotation-z={Math.PI / 2} position={spring.crank}>
+              <cylinderGeometry args={[0.07, 0.07, 1, 32]} />
+              <meshPhongMaterial />
+            </animated.mesh>
             <animated.mesh position={spring.bearing}>
               <mesh rotation-z={Math.PI / 2}>
                 <cylinderGeometry args={[0.1, 0.1, 0.15, 32]} />
                 <meshPhongMaterial color="grey" />
               </mesh>
             </animated.mesh>
-            <animated.mesh position={spring.tank}>
+            <animated.mesh position={spring.bearing2}>
+              <mesh rotation-z={Math.PI / 2}>
+                <cylinderGeometry args={[0.1, 0.1, 0.15, 32]} />
+                <meshPhongMaterial color="grey" />
+              </mesh>
+            </animated.mesh>
+            <animated.mesh
+              position={spring.tank}
+              rotation-y={spring.tankRotation}
+            >
               <boxGeometry args={[0.5, 0.25, 0.25]} />
               <meshPhongMaterial color="white" />
             </animated.mesh>
@@ -182,6 +220,14 @@ const Home = () => {
 
             <AnimatedLine
               points={diagram ? pointsDiagram : pointsDiagram}
+              opacity={diagram ? 1 : 0}
+              style="dotted"
+              color="grid"
+            />
+
+            <AnimatedLine
+              points={pointsDiagram2}
+              opacity={diagram ? 1 : 0}
               style="dotted"
               color="grid"
             />
