00de64eef1e29fae95a1967662a7703682047d85
adamsmietanka <adamsmietanka@outlook.com>
2024-03-18 14:32:59 +0100

    feat(wing): add basic elliptical wing control surfaces

--- frontend/src/features/aerodynamics/utils/wing/Wing.spec.ts
+++ frontend/src/features/aerodynamics/utils/wing/Wing.spec.ts
@@ -24,4 +24,22 @@ describe("Wing creation", () => {
 
     expect(wing.sectionPoints(0.95, 1)).toEqual([0.95, 1]);
   });
+
+  it("creates elliptical section points", () => {
+    const wing = new Wing({ ...config, shape: 2 }, "30");
+
+    expect(wing.sectionPoints(0, 0.5)).toEqual([
+      0, 0.1305261922200516, 0.2588190451025208, 0.3826834323650898, 0.5,
+    ]);
+
+    expect(wing.sectionPoints(0.2, 0.5)).toEqual([
+      0.20000000000000004, 0.30388850196046313, 0.40427420378600176, 0.5,
+    ]);
+
+    expect(wing.sectionPoints(0.9, 1)).toEqual([
+      0.9, 0.9551339695515183, 0.9887198717411111, 1,
+    ]);
+
+    expect(wing.sectionPoints(0, 1).length).toBe(11);
+  });
 });
