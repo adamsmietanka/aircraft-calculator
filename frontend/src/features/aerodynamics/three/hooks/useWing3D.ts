84232f54380594074b1ef9fa556d3c8558005e7d
adamsmietanka <adamsmietanka@outlook.com>
2024-11-24 23:28:10 +0200

    feat(wing): add debouncing to transform

--- frontend/src/features/aerodynamics/three/hooks/useWing3D.ts
+++ frontend/src/features/aerodynamics/three/hooks/useWing3D.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useWingStore } from "../../stores/useWing";
 import { getStep } from "../../../common/three/hooks/useAxes";
+import debounce from "../../../../utils/debounce";
 
 const getXTip = (angle: number, span: number) =>
   (Math.tan((angle * Math.PI) / 180) * span) / 2;
@@ -16,26 +17,38 @@ const useWing3D = () => {
     }
   }, [wing.shape]);
 
-  const onTransform = (e: THREE.Event | undefined) => {
-    if (e && e.target.object) {
-      const { isTip, isTrailing, isFuselage } = e.target.object.userData;
-      const { x, y } = e.target.object.position;
+  const update = useCallback(
+    debounce(
+      (
+        userData: { isTip: boolean; isTrailing: boolean; isFuselage: boolean },
+        x: number,
+        y: number
+      ) => {
+        if (userData.isTip) {
+          useWingStore.setState({ span: parseFloat((y * 2).toPrecision(4)) });
 
-      if (isTip) {
-        useWingStore.setState({ span: parseFloat((y * 2).toPrecision(4)) });
+          if (userData.isTrailing) {
+            const angle = useWingStore.getState().angle;
+            const span = useWingStore.getState().span;
+            const xTip = getXTip(angle, span);
 
-        if (isTrailing) {
-          const angle = useWingStore.getState().angle;
-          const span = useWingStore.getState().span;
-          const xTip = getXTip(angle, span);
-
-          useWingStore.setState({
-            chordTip: parseFloat((x - xTip).toPrecision(4)),
-          });
+            useWingStore.setState({
+              chordTip: parseFloat((x - xTip).toPrecision(4)),
+            });
+          }
+        } else if (userData.isFuselage) {
+          useWingStore.setState({ chord: parseFloat(x.toPrecision(4)) });
         }
-      } else if (isFuselage) {
-        useWingStore.setState({ chord: parseFloat(x.toPrecision(4)) });
-      }
+      },
+      33
+    ),
+    []
+  );
+
+  const onTransform = (e: THREE.Event | undefined) => {
+    if (e && e.target && e.target.object) {
+      const { x, y } = e.target.object.position;
+      update(e.target.object.userData, x, y);
     }
   };
 
